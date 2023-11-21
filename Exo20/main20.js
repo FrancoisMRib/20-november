56d7b7fd1ad6b09810e8689ecee3d6cad39a2dcb
FracoisMRib <fr.ribere@gmail.com>
2023-11-21 12:19:42 +0100

    Exo22

--- Exo20/main20.js
+++ Exo20/main20.js
@@ -43,6 +43,14 @@ const premierPara = document.querySelector('div > p');
 premierPara.textContent = user.nom ;
 console.log(premierPara)
 
+//Idem, mais avec la commande nth-child :
+const p1 = document.querySelector('div>p:nth-child(2)')
+// Il est marqué nth-child(2) car c'est le 2e enfant de la section div, après l'image.
+//version alternative pour récupérer les paragraphes
+const p1bis = div.firstChild.nextSibling ;
+const p2bis = p1bis.nextSibling ;
+const p3bis = p2bis.nextSibling ;
+
 const deuxiemePara = document.querySelector('div > p+p');
 deuxiemePara.textContent = user.prenom ;
 
@@ -51,13 +59,20 @@ troisiemePara.textContent = user.age ;
 
 //CORRECTION :
 //version nodelist
-const liste = document.querySelectorAll('p');
+//const liste = document.querySelectorAll('p');
 //versio html
-const liste2 = document.getElementsByTagName('p');
-liste[0].textContent = user.nom ;
+//const liste2 = document.getElementsByTagName('p');
+//liste[0].textContent = user.nom ;
 
 //Remplacer l'image par l'url contenu dans user.image
 //user.textContent = https://cdn.discordapp.com/attachments/1151090232683405324/1176117311510433832/logo.jpg?ex=656db39a&is=655b3e9a&hm=427eaccaf3e63024114fdd9c79b62fc9a8ae1ccebcdd9cf5ac128c2a0ab85b0e& ;
+
+//VERSION CORRECTION
+//const img = document.querySelector('div>img');
+//modifier setAttribute
+//img.setAttribute('src', user.image);
+//modifier la source avec src
+//img.src = user.image ;
 
 //Mettre en forme les éléments suivants : paragraphe nom, prenom, age : police taille 11 px, couleur blue,
 
@@ -70,6 +85,13 @@ deuxiemePara.style.color = 'blue';
 troisiemePara.style.fontSize = '11px' ;
 troisiemePara.style.color = 'blue';
 
+/*avec une boucle
+liste.forEach(paragraphe => {
+  paragraphe.style.color = "blue";
+  paragraphe.style.fontSize = "11px";
+});
+*/
+
 // taille de l'image : 100 px de large et 100 px de long
 
 const tailleImage = document.querySelector('div > img');
@@ -78,5 +100,11 @@ tailleImage.style.height = '100px';
 
 //Aligner l'image à en haut à droite et ajouter un margin de 10 px.
 
-//tailleImage.style.right = ;
-tailleImage.style.margin= '10px';
+tailleImage.style.top = '50px' ;
+//tailleImage.style.left = 0 ;
+tailleImage.style.right = 0 ;
+tailleImage.style.margin = '10px';
+
+//couleur de la div
+div.style.background = "rgb(220, 220, 220)";
+div.style.height = "200px"
